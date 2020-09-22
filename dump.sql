7488fec71e2ac91c408fd6167643259e983a2b6c
ayush-dg07 <43674999+ayush-dg07@users.noreply.github.com>
2020-09-22 17:57:23 +0530

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,4 +1,2 @@
-create database if not exists simpleLogin;
-use simpleLogin;
 drop table if exists users;
 create table users( uid int not null auto_increment, gid text, name text, regdate date, primary key(uid) );
